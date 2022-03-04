3d820cf1c0d79adc37fa0e4c486ac4dd0ff7f6dd
Hector <contact@salaxer.com>
2022-03-04 11:59:45 -0600

    comunicacion con el front realizada

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const app = require('express')();
+// const http = require('http').Server(app);
+// const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('dataTranfer', data => {
+    io.emit('dataTranfer', data);
   });
 });
 
-http.listen(port, () => {
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);      // the request object
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
+});
+
+
+httpServer.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
